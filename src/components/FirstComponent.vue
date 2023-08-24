b03687ac0ab103c77d550aa1051442ed93934fd8
Leandro-Oliveira-2 <leandro.oliveira@dcx.ufpb.br>
2023-08-24 17:12:37 -0300

    Implements fist version project

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Primeiro template a dar certo</h1>
+    <p>Digite se nome;</p>
+    <input v-model="nome" type="text">
+    <p>Digite Sua profissão;</p>
+    <input v-model="profissao" type="text">
+    <h1>Olá meu nome é {{ nome }} e minha profissão é {{ profissao }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'FirtComponent',
+    data () {
+        return {
+            nome: '',
+            profissao: ''
+        }
+    }
+}
+</script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <input v-model="nome" type="text">
+    <h1>Meu nome é: {{ nome }} </h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data () {
+        return {
+            nome: "ainda não sei"
+        }
+    },
+    created() {
+        setTimeout(()=>{
+            this.nome = "Leandro"
+        }, 2000);
+    },
+    mounted() {
+        setTimeout(()=>{
+            this.nome = "Pedro"
+        }, 1000);
+    }
+}
+    
+</script>
